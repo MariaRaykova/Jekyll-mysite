b08bdfd70c424078d044b553f6c877c99a78ecbd
Maria Raykova <mimi.raykova@gmail.com>
2021-02-22 12:00:21 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -23,12 +23,12 @@ gulp.task("css", function() {
 
 // Jekyll
 gulp.task("jekyll-dev", function() {
-	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl ''"], { stdio: "inherit", shell: true });
+	return cp.spawn("bundle", ["exec", "jekyll", "build "], { stdio: "inherit", shell: true });
 });
 
 // Jekyll
-gulp.task("jekyll", function() {
-	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+gulp.task("jekyll-prod", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl /Jekyll-mysite"], { stdio: "inherit", shell: true });
 });
 
 gulp.task("watch", function() {
@@ -56,11 +56,8 @@ gulp.task("watch", function() {
 });
 
 
-gulp.task("deploy", gulp.series('jekyll', 'css', function() {
+gulp.task("deploy", gulp.series('jekyll-prod', 'css', function() {
 	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
 }));
-gulp.task('talktome', function() {
-	console.log('Hello From Zestcode');
-	});
 
 gulp.task("default", gulp.series('jekyll-dev', 'css', 'watch'));
